a62135a0382a23bb2af0e331e04f9762844c119e
Levi-musleh <Levi-Musleh-A@outlook.com>
2023-07-08 04:20:06 +0300

    setup branch

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const process = require("process");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//Routes
+
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+app.listen(PORT, () =>
+  console.log(`server is live on http://localhost:${PORT}`)
+);
